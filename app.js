d3ad0eccc3d835f4dea965b32e5b1d25b008adbf
cverhulle <corentin.verhulle@gmail.com>
2025-02-11 10:54:02 +0100

    Connexion à MongoDB.

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 
+// Connexion à MongoDB
+mongoose.connect('mongodb+srv://cverhulle:TestOC212683@discord.hcb4b.mongodb.net/?retryWrites=true&w=majority&appName=Discord',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+// Résoudre les problèmes de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+// Réponse du serveur
 app.use((req, res) => {
     res.json({ message: 'Your request was successful!' });
 })
 
+
 module.exports = app;
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
+// normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -18,6 +19,7 @@ const port = normalizePort(process.env.PORT ||'3000');
 app.set('port', port);
 
 
+// errorHandler recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -40,11 +42,13 @@ const errorHandler = error => {
 
 const server = http.createServer(app);
 
+// un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
 server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
 });
+
 
 server.listen(port);
