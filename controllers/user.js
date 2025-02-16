e73844a4a226c12ff8f855916f2f7dc565894886
cverhulle <corentin.verhulle@gmail.com>
2025-02-16 18:07:52 +0100

    Sauvegarde du hashage dans la création de compte

--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,15 @@ const User = require('../models/user');
 
 
 exports.register = (req, res, next) => {
+    // Création d'un nouvel utilisateur dans la base de données.
+
+    // Hashage du mot de passe
     bcrypt.hash(req.body.login['password'], 10)
-      .then(hash => {
+    .then(hash => {
+        // On remplace les mots de passe en clair par le mot de passe hashé.
+        req.body.login['password'] = hash;
+        req.body.login['confirmPassword'] = hash;
+        // Création d'un nouvel utilisateur avec le modèle mongoose et le mdp  hashé.
         const user = new User({
           personalInfo: req.body.personalInfo,
           emailInfo: req.body.email,
@@ -23,8 +30,8 @@ exports.register = (req, res, next) => {
   
 
 exports.login = (req, res, next) => {
-    // Trouver l'utilisateur dans la base de données (username et pas email)
-   User.findOne({ email: req.body.email })
+
+   User.findOne({ username: req.body.login['username'] })
        .then(user => {
            if (!user) {
                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
