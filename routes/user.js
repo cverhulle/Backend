a4bca076214c4d1b86cde7c64d128605e2d564cf
cverhulle <corentin.verhulle@gmail.com>
2025-02-18 22:43:19 +0100

    Ajout des routes pour emailExists et usernameExists.

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,8 @@ const userCtrl = require('../controllers/user');
 const multer = require('../middleware/multer-config')
 
 
-
+router.post('/register/email', userCtrl.emailExists)
+router.post('/register/username', userCtrl.usernameExists)
 router.post('/register', userCtrl.register);
 router.post('/login', userCtrl.login);
 
