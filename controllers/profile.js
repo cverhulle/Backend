3c2b42df1f30c51a658ea3200576f7adc056ee77
cverhulle <corentin.verhulle@gmail.com>
2025-02-21 14:41:11 +0100

    Création de la méthode getProfile qui retourne les infos de l'utilisateur à l'id fixé.

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,3 +1,10 @@
+const User = require ('../models/user')
 
+exports.getProfile = (req, res, next) => {
+    User.findOne( {_id : '67b5d72d26a7c6922f7db32f'})
+        .then( (user) =>
+            res.status(200).json({user})
+        )
 
-exports.getProfile = (req, res, next) => {}
+        .catch(error => res.status(400).json({error}))
+}
