c1c61c6e9fedc19e6576206de0df1078554ff3d4
cverhulle <corentin.verhulle@gmail.com>
2025-02-21 14:44:59 +0100

    Ajout d'un commentaire.

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,6 @@
 const User = require ('../models/user')
 
+// Cette méthode retourne les infos sur l'utilisteur à l'id fixé.
 exports.getProfile = (req, res, next) => {
     User.findOne( {_id : '67b5d72d26a7c6922f7db32f'})
         .then( (user) =>
