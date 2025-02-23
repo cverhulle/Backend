bf3efd0be6b818acbc2f38d013e2a163576b375d
cverhulle <corentin.verhulle@gmail.com>
2025-02-23 18:41:33 +0100

    Suppression de l'import de multer : pas utilisé pour le moment.

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
-const multer = require('../middleware/multer-config')
+
 
 
 router.post('/register/email', userCtrl.emailExists)
