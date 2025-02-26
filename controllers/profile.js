44da2fabea307457ba4e83decec45e40e4ce9568
cverhulle <corentin.verhulle@gmail.com>
2025-02-26 14:53:12 +0100

    Création et test de la méhode modifyProfile pour modifier le profil. Attenion, le mdp est modifié et non crypté.

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,7 +2,7 @@ const User = require ('../models/user')
 
 // Cette méthode retourne les infos sur l'utilisteur à l'id fixé.
 exports.getProfile = (req, res, next) => {
-    User.findOne( {_id : req.auth['userId']})
+    User.findOne( {_id : req.auth['userId'] })
         .then( (user) =>
             res.status(200).json({user})
         )
@@ -11,6 +11,22 @@ exports.getProfile = (req, res, next) => {
 }
 
 exports.modifyProfile = (req, res, next) => {
-    console.log('test')
-    res.status(200).json({message : 'ok'})
+    
+    const {personalInfo, email, login, image} = req.body
+
+    const updateData = {
+        'personalInfo.firstName': personalInfo.firstName,
+        'personalInfo.lastName': personalInfo.lastName,
+        'emailInfo.email':email.email,
+        'emailInfo.confirmEmail': email.confirmEmail,
+        'loginInfo.username': login.username,
+        'loginInfo.password': login.password,
+        'loginInfo.confirmPassword': login.confirmPassword,
+        'image': image
+    }
+
+    console.log(updateData)
+    User.updateOne( {_id : req.auth['userId'] }, updateData)
+        .then((user) => res.status(200).json({message : "L'utilisateur a été modifié"}))
+        .catch(error => res.status(400).json({error}))
 }
