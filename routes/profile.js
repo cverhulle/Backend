a93a64e9d44a24c98d9ae50fca114363c50a7ae8
cverhulle <corentin.verhulle@gmail.com>
2025-03-03 18:06:18 +0100

    Modification du post en put pour la modification de mot de passe.

--- routes/profile.js
+++ routes/profile.js
@@ -7,7 +7,7 @@ const auth = require('../middleware/auth');
 
 router.get('', auth, profileCtrl.getProfile);
 router.put('/modify', auth, profileCtrl.modifyProfile)
-router.post('/modifyPassword', auth, profileCtrl.modifyPassword)
+router.put('/modifyPassword', auth, profileCtrl.modifyPassword)
 router.post('/email', auth, profileCtrl.emailTaken)
 router.post('/username', auth, profileCtrl.usernameTaken)
 
