30f45cd69350c408417bdd67b13011947d5b0464
cverhulle <corentin.verhulle@gmail.com>
2025-03-05 16:46:13 +0100

    Modification de la méthode queryUsers pour qu'elle ne retourne pas l'utilisateur qui fait la requete.

--- controllers/private-message.js
+++ controllers/private-message.js
@@ -1,13 +1,13 @@
 const User = require ('../models/user')
 
-// Cette méthode retourne les utilisateurs en fonction de la requete formulée.
+// Cette méthode retourne les utilisateurs, à l'exception de celui qui fait la requete, en fonction de l'entrée formulée.
 exports.queryUsers = (req, res, next) => {
     // On récupère le paramètre de recherche que l'on met en minuscule.
     const searchQuery = req.query.search;
     
     // Le $regex permet de chercher les username contenant la recherche formulée.
     // Options permet d'être insensible à la casse : Co, co et CO seront traités de la même façon.
-    User.find({'loginInfo.username' : {$regex: searchQuery, $options : 'i'} })
+    User.find({'loginInfo.username' : {$regex: searchQuery, $options : 'i'}, _id: {$ne : req.auth['userId']}  })
 
         // On selectionne les champs à retourner
         .select('loginInfo.username image')
