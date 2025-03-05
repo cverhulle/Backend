00d8e456fe4040aa68fe515406d08bbfce047f48
cverhulle <corentin.verhulle@gmail.com>
2025-03-05 12:06:31 +0100

    Implémentation de la méthode queryUsers.

--- controllers/private-message.js
+++ controllers/private-message.js
@@ -1,6 +1,16 @@
-
+const User = require ('../models/user')
 
 
 exports.queryUsers = (req, res, next) => {
-    console.log(req.query.search)
+    // On récupère le paramètre de recherche que l'on met en minuscule.
+    const searchQuery = req.query.search;
+    
+
+    User.find({'loginInfo.username' : {$regex: searchQuery, $options : 'i'} })
+        .then(users => {
+            console.log(users)
+            res.status(200).json(users)
+        })
+        .catch(error => res.status(500).json({error}))
+
 }
