434ccafed24c29959647f03435ddebf0ab75bafa
cverhulle <corentin.verhulle@gmail.com>
2025-03-05 12:08:20 +0100

    Ajout de commentaires.

--- controllers/private-message.js
+++ controllers/private-message.js
@@ -1,11 +1,12 @@
 const User = require ('../models/user')
 
-
+// Cette méthode retourne les utilisateurs en fonction de la requete formulée.
 exports.queryUsers = (req, res, next) => {
     // On récupère le paramètre de recherche que l'on met en minuscule.
     const searchQuery = req.query.search;
     
-
+    // Le $regex permet de chercher les username contenant la recherche formulée.
+    // Options permet d'être insensible à la casse : Co, co et CO seront traités de la même façon.
     User.find({'loginInfo.username' : {$regex: searchQuery, $options : 'i'} })
         .then(users => {
             console.log(users)
