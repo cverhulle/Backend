de04ae19ffb5fae1864d7d0189b56b4620a4bca2
cverhulle <corentin.verhulle@gmail.com>
2025-03-05 11:40:23 +0100

    Création du controller private-message. Implémentation du chemin dans la route private-message.

---
+++ routes/private-message.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const privateMessageCtrl = require('../controllers/private-message') ;
+
+
+
+
+
+
+
+module.exports = router;
