c08246113080af3a8ffd84727ef6a10fe75e15f2
cverhulle <corentin.verhulle@gmail.com>
2025-03-07 11:30:58 +0100

    Ajout de otherUserId dans le model.

--- models/post.js
+++ models/post.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    id: {type : String, required : true, unique : true},
+    currentUserId: {type : String, required : true, unique : true},
+    otherUserId: {type : String, required : true, unique : true},
     username: {type : String, required : true, unique : true},
     image: {type : String, required : true},
     content: {type : String, required : true},
