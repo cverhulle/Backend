c4da119f7f5d18f1c8f1f30dc776580b707934ac
cverhulle <corentin.verhulle@gmail.com>
2025-03-11 11:29:15 +0100

    Ajout de la route pour récuprer les previous posts.

--- routes/private-message.js
+++ routes/private-message.js
@@ -7,6 +7,7 @@ const privateMessageCtrl = require('../controllers/private-message') ;
 
 router.get('/queryUsers', auth, privateMessageCtrl.queryUsers)
 router.get('/getPosts', auth, privateMessageCtrl.getPosts)
+router.get('/getPosts/previous', auth, privateMessageCtrl.getPreviousPosts)
 router.post('/post', auth, privateMessageCtrl.savePost)
 
 
