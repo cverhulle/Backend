c0f5d3d86fe0397a4674a1b8c80a333088130d78
cverhulle <corentin.verhulle@gmail.com>
2025-03-24 16:56:51 +0100

    Ajout de la route pour les posts avec une image.

--- routes/private-message.js
+++ routes/private-message.js
@@ -9,6 +9,7 @@ router.get('/queryUsers', auth, privateMessageCtrl.queryUsers)
 router.get('/getPosts', auth, privateMessageCtrl.getPosts)
 router.get('/getPosts/previous', auth, privateMessageCtrl.getPreviousPosts)
 router.post('/post', auth, privateMessageCtrl.savePost)
+router.post('/send-message-image', auth, privateMessageCtrl.savePost)
 router.delete('/deletePost', auth, privateMessageCtrl.deletePost)
 router.put('/updatePost', auth, privateMessageCtrl.updatePost)
 
