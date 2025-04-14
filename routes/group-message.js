9c2b952720b1f7970c4707be22f5eb93b13db944
cverhulle <corentin.verhulle@gmail.com>
2025-04-14 12:23:01 +0200

    Ajout du controller sur la router.

--- routes/group-message.js
+++ routes/group-message.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const groupMessageCtrl = require('../controllers/group-message')
 
-
-router.post('/create-group', auth, uploadImages)
+router.post('/create-group', auth, uploadImages, groupMessageCtrl.createGroup)
 
 
 module.exports = router;
