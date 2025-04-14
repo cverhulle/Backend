ca61a683ddde4e1fa7e0b68c42b9cce4bfab6dc5
cverhulle <corentin.verhulle@gmail.com>
2025-04-14 17:08:45 +0200

    On gère le chemin de l'image.

--- controllers/group-message.js
+++ controllers/group-message.js
@@ -19,4 +19,8 @@ exports.createGroup = (req,res,next) => {
     if (!groupName || !groupDescription || !groupType || !groupLanguages || !groupCategories || !fullPath) {
          return res.status(400).json({ message: 'Tous les champs doivent être remplis.' });
     }
+
+    // On gère le chemin de l'image (pour la récupèrer depuis le serveur localhost:\\3000)
+    const imageName = path.basename(fullPath);
+    const groupLogo = `http://localhost:3000/images/${imageName}`;
 }
