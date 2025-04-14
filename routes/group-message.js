bde908cb251422a68fc402e21448c0246d11414b
cverhulle <corentin.verhulle@gmail.com>
2025-04-14 12:21:01 +0200

    Ajout de la route create-group.

--- routes/group-message.js
+++ routes/group-message.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 
+
+router.post('/create-group', auth, uploadImages)
+
+
 module.exports = router;
