c6fb2931b4370159e9319a153131791415247095
cverhulle <corentin.verhulle@gmail.com>
2025-04-14 16:56:23 +0200

    Ajout de commentaires.

--- models/group.js
+++ models/group.js
@@ -1,40 +1,46 @@
 const mongoose = require('mongoose');
 
 const groupMessageSchema = new mongoose.Schema({
+    // On sauvegarde le nom du groupe.
     groupName: {
         type: String,
         required: true,
-
         // Permet de retirer les espaces vides
         trim: true
     },
+    // On sauvegarde la description du groupe.
     groupDescription: {
         type: String,
         required: true,
-
         // Permet de retirer les espaces vides
         trim: true
     },
+    // On sauvegarde le type du groupe.
     groupType: {
         type: String,
         enum: ['Public', 'Privé', 'Restreint'],
         required: true
     },
+    // On sauvegarde le mot de passe (il n'y en a un que si le groupe est de type Restreint)
     groupPassword: {
         type: String,
         required: function() {
         return this.groupType === 'Restreint';
         }
     },
+    // On sauvegarde le tableau des languages
     groupLanguages: {
         type: [String],
     },
+    // On sauvegarde le tableau des catégories
     groupCategories: {
         type: [String],
     },
+    // On sauvegarde le logo du group (le path)
     groupLogoPath: {
         type: String
     },
+    // On sauvegarde la date de création du groupe
     createdAt: {
         type: Date,
         default: Date.now
