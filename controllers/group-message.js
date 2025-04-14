46be4a89c1241c6a8b7cf196b4721ff2dba4d7ee
cverhulle <corentin.verhulle@gmail.com>
2025-04-14 17:00:33 +0200

    On récupère les données de req.body

--- controllers/group-message.js
+++ controllers/group-message.js
@@ -2,5 +2,13 @@ const GroupMessage = require('../models/group')
 const path = require('path')
 
 exports.createGroup = (req,res,next) => {
-    console.log(req.body)
+    const {
+        groupName,
+        groupDescription,
+        groupType,
+        groupLanguages,
+        groupCategories,
+        groupPassword
+    } = req.body
+
 }
