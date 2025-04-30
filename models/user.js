e1c96a4745b39f1b815dc7bd1d6880248e90f4ac
cverhulle <corentin.verhulle@gmail.com>
2025-04-30 17:46:25 +0200

    Ajout de commentaires

--- models/user.js
+++ models/user.js
@@ -1,21 +1,29 @@
 const mongoose = require('mongoose');
 
 
-
+// Ce modèle est utilisé pour sauvegarder les données des utilisateurs
 const userSchema = mongoose.Schema({
+    
+    // Nom et prénom de l'utilisateur
     personalInfo: { 
         firstName: { type: String, required: true},
         lastName: { type: String, required: true},
     },
+
+    // Email de l'utilisateur 
     emailInfo: { 
         email: { type: String, required: true, unique: true},
         confirmEmail: { type: String, required: true},
     },
+
+    // Pseudo et mot de passe de l'utilisateur
     loginInfo: {
         username: { type: String, required: true, unique: true},
         password: { type: String, required: true},
         confirmPassword: { type: String, required: true},
     },
+
+    // URL de Image de profil de l'utlisateur l'
     image: { type: String, required: true},
 });
 
