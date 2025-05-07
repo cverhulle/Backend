f59f1bc417885403f7664b0802e179dbeceeb9fd
cverhulle <corentin.verhulle@gmail.com>
2025-05-07 11:47:17 +0200

    Ajout d'un commentaires sur chaque route

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 
 
-
+// Route permettant si un utilisateur possède déjà l'adresse email
 router.post('/register/email', userCtrl.emailExists)
+
+// Route permettant de vérifier si un username est déjà utilisé
 router.post('/register/username', userCtrl.usernameExists)
+
+// Route permettant de créer un compte
 router.post('/register', userCtrl.register);
+
+// Route permettant de se connecter
 router.post('/login', userCtrl.login);
 
 
