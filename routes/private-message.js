1b3b7a28d8e85a15d3d2443b7fb7462891b13a8b
cverhulle <corentin.verhulle@gmail.com>
2025-05-07 11:43:06 +0200

    Ajout de commentaires sur chaque route

--- routes/private-message.js
+++ routes/private-message.js
@@ -6,12 +6,22 @@ const uploadImages = require('../middleware/uploadImages')
 
 
 
-
+// Route permettant de chercher les utilisateurs sur la homepage
 router.get('/queryUsers', auth, privateMessageCtrl.queryUsers)
+
+// Route permettant de récupérer les posts d'une discussion
 router.get('/getPosts', auth, privateMessageCtrl.getPosts)
+
+// Route permettant de récupérer des posts à l'appui sur le bouton "Plus de messages"
 router.get('/getPosts/previous', auth, privateMessageCtrl.getPreviousPosts)
+
+// Route permettant de sauvegarder le nouveau Post
 router.post('/send-message', auth, uploadImages, privateMessageCtrl.savePost)
+
+// Route permettant de supprimer un post
 router.delete('/deletePost', auth, privateMessageCtrl.deletePost)
+
+// Route permettant de mettre à jour un post
 router.put('/updatePost', auth, uploadImages, privateMessageCtrl.updatePost)
 
 
